d269137376db499a8f9471ad0ee8bce921b02ff1
Umer Mansoor <umer58984@gmail.com>
2026-03-17 08:00:12 +0500

    fix: CORS update for Netlify

--- Backend/index.js
+++ Backend/index.js
@@ -14,10 +14,19 @@ const app = express();
 const uploadsDir = path.join(process.cwd(), "uploads");
 if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir, { recursive: true });
-  console.log("uploads folder created!");
 }
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// CORS fix
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://umer-fitness-tracker.netlify.app",
+    "https://rad-cactus-940a78.netlify.app",
+    "https://euphonious-alfajores-903c6f.netlify.app",
+  ],
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
@@ -28,7 +37,7 @@ app.use("/api/workout", workoutRouter);
 app.get("/", (req, res) => res.send("Fitness Tracker API Running"));
 
 connectDB().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on port ${process.env.PORT || 3000}`);
   });
 });
